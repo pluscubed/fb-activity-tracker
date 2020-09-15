3beb9e93141e73bb8605a418a52e0ecf0d84b08f
plusCubed <pluscubed@users.noreply.github.com>
2020-09-15 04:21:40 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,144 @@
+const { JSDOM } = require("jsdom");
+const FormData = require("form-data");
+const nodeFetch = require("node-fetch");
+const fetch = require("fetch-cookie/node-fetch")(nodeFetch);
+const fs = require("fs");
+const nodeCleanup = require("node-cleanup");
+const cred = require("./cred.json");
+
+const DB_FILE = "./viewer/db.json";
+
+if (!Array.prototype.last) {
+  Array.prototype.last = function () {
+    return this[this.length - 1];
+  };
+}
+
+const db = fs.existsSync(DB_FILE)
+  ? JSON.parse(fs.readFileSync(DB_FILE))
+  : { recorded: [], users: {} };
+
+const startTime = new Date().getTime();
+
+const onExit = () => {
+  console.log("saving...");
+  db.recorded.push([startTime, new Date().getTime()]);
+  fs.writeFileSync(DB_FILE, JSON.stringify(db));
+};
+
+nodeCleanup(onExit);
+
+async function main() {
+  const options = {
+    //timeout: 20000,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      Connection: "keep-alive",
+      "Cache-Control": "no-cache",
+    },
+    encoding: null,
+  };
+  const loginFormData = new FormData();
+  loginFormData.append("email", cred.email);
+  loginFormData.append("pass", cred.pass);
+  console.log("login sent");
+  const resp = await fetch(
+    "https://mbasic.facebook.com/login/device-based/regular/login/",
+    {
+      ...options,
+      method: "POST",
+      body: loginFormData,
+    }
+  );
+  //console.log(resp);
+  console.log("logged in");
+  await sleep(4000);
+
+  // const frontPage = await fetch("https://mbasic.facebook.com/", options);
+  // const frontPageHtml = await frontPage.text();
+  // console.log(frontPageHtml)
+
+  while (true) {
+    const date = new Date();
+    console.log(`${date.toLocaleDateString()} ${date.toLocaleTimeString()}`);
+    const logTime = date.getTime();
+
+    const buddyList = await fetch("https://mbasic.facebook.com/buddylist.php");
+    const buddyListHtml = await buddyList.text();
+    //console.log(buddyListHtml);
+    const { document } = new JSDOM(buddyListHtml, {
+      pretendToBeVisual: true,
+    }).window;
+
+    fs.writeFileSync("debug.html", buddyListHtml);
+
+    const rows = document.querySelectorAll("table");
+    const seenThisTime = [];
+    for (const row of rows) {
+      //console.log("processing");
+      if (row.classList.length !== 3) {
+        continue;
+      }
+      const link = row.querySelector("a");
+      const img = row.querySelector("img");
+      if (!link || !img) {
+        continue;
+      }
+
+      const fbId = /fbid=(\d+)/.exec(link.href)[1];
+      const name = link.textContent;
+      if (!(fbId in db.users)) {
+        db.users[fbId] = { name, seen: [] };
+      }
+      db.users[fbId].name = name;
+
+      const active =
+        img.src ===
+        "https://static.xx.fbcdn.net/rsrc.php/v3/yU/r/gATt-jY8pG8.png"; //img.ariaLabel === "Active Now";
+
+      if (active) {
+        const lastLog = db.users[fbId].seen.last();
+        if (!lastLog || lastLog[1] !== true) {
+          db.users[fbId].seen.push([logTime, true]);
+        }
+        seenThisTime.push(fbId);
+        console.log("   " + name);
+      }
+    }
+    for (const fbId of Object.keys(db.users)) {
+      if (!seenThisTime.includes(fbId)) {
+        // console.log(db.users);
+        const lastLog = db.users[fbId].seen.last();
+        if (!lastLog || lastLog[1] !== false) {
+          db.users[fbId].seen.push([logTime, false]);
+        }
+      }
+    }
+
+    fs.writeFileSync(DB_FILE, JSON.stringify(db));
+
+    const sleepTime = 60000 + Math.random() * 120000;
+    console.log(`sleeping ${sleepTime / 1000}s`);
+    await sleep(sleepTime);
+  }
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+main()
+  .then(() => {
+    console.log("complete");
+  })
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(() => {
+    onExit();
+  });
---
+++ viewer/main.js
@@ -0,0 +1,120 @@
+import htm from "https://unpkg.com/htm?module";
+import React from "https://cdn.skypack.dev/react";
+import ReactDOM from "https://cdn.skypack.dev/react-dom";
+const html = htm.bind(React.createElement);
+
+import { AutoSizer, List } from "https://cdn.skypack.dev/react-virtualized";
+
+class App extends React.Component {
+  state = {
+    entries: [],
+  };
+
+  async componentDidMount() {
+    const resp = await fetch("./db.json");
+    const db = await resp.json();
+
+    const entries = Object.entries(db.users);
+    entries.sort(([idA, userA], [idB, userB]) => {
+      return userA.name.localeCompare(userB.name);
+    });
+
+    let earliestTimestamp = Number.MAX_VALUE;
+    let latestTimestamp = Number.MIN_VALUE;
+    for (const [id, user] of entries) {
+      earliestTimestamp = Math.min(user.seen[0][0], earliestTimestamp);
+      latestTimestamp = Math.max(
+        user.seen[user.seen.length - 1][0],
+        latestTimestamp
+      );
+    }
+
+    console.log(earliestTimestamp, latestTimestamp);
+
+    for (const [id, user] of entries) {
+      const seen = user.seen;
+      if (user.seen[0][0] !== earliestTimestamp) {
+        user.seen.splice(0, 0, [earliestTimestamp, false]);
+      }
+
+      let lastActive = null;
+      for (let i = 0; i < seen.length; i++) {
+        const [timestamp, active] = seen[i];
+        if (lastActive === active) {
+          seen.splice(i, 1);
+          i--;
+        }
+        lastActive = active;
+      }
+    }
+
+    this.setState({ entries, earliestTimestamp, latestTimestamp });
+  }
+
+  rowRenderer = ({
+    key, // Unique key within array of rows
+    index, // Index of row within collection
+    style, // Style object to be applied to row (to position it)
+  }) => {
+    const [id, user] = this.state.entries[index];
+    const earliestDate = new Date(this.state.earliestTimestamp);
+    const latestDate = new Date(this.state.latestTimestamp);
+    return html`
+      <div key=${id} className="p-2 row" style="${style}">
+        <div>
+          <!--className="max-w-screen-md mx-auto"-->
+          <div className="font-bold">${user.name}</div>
+          <div className="relative overflow-hidden active-block-container">
+            ${user.seen.map(([timestamp, active]) => {
+              const color = active ? "green" : "#e2e8f0";
+              const left =
+                ((timestamp - this.state.earliestTimestamp) /
+                  (this.state.latestTimestamp - this.state.earliestTimestamp)) *
+                100;
+              return html`
+                <div
+                  className="absolute active-block"
+                  style=${{ backgroundColor: color, left: left + "%" }}
+                ></div>
+              `;
+            })}
+          </div>
+          <div className="flex">
+            <div className="date-label inline-block text-left">
+              ${earliestDate.toLocaleDateString() +
+              " " +
+              earliestDate.toLocaleTimeString()}
+            </div>
+            <div className="flex-1"></div>
+            <div className="date-label inline-block text-right">
+              ${latestDate.toLocaleDateString() +
+              " " +
+              latestDate.toLocaleTimeString()}
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  };
+
+  render() {
+    return html`
+      <div className="h-full app">
+        <${AutoSizer}>
+          ${({ height, width }) => html`
+            <${List}
+              height="${height}"
+              rowCount="${this.state.entries.length}"
+              rowHeight="${84}"
+              rowRenderer="${this.rowRenderer}"
+              width="${width}"
+              overscanRowCount="${30}"
+            />
+          `}
+        </${AutoSizer}>
+      </div>
+    `;
+  }
+}
+
+ReactDOM.render(html`<${App} />`, document.body);
